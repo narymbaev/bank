40ff42854f865c0da51b3e12ab1ad07cb2dacdd9
nurm <narymbaev.n02@gmail.com>
2023-12-26 20:16:09 +0600

    cleanup

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -21,11 +21,11 @@ func TestTransferTx(t *testing.T) {
 
 	errs := make(chan error)
 
-	for i := 0; i < n; i ++ {
+	for i := 0; i < n; i++ {
 		fromAccountID := account1.ID
 		toAccountID := account2.ID
 
-		if i % 2 == 1 {
+		if i%2 == 1 {
 			fromAccountID = account2.ID
 			toAccountID = account1.ID
 		}
